6ea44f430b9be148f6dafe16fc3d1f88042288c1
Alezzo <alejandrommp4.oficial@gmail.com>
2023-05-17 08:44:20 -0500

    Agregando la parte de tipos de msenaje

--- src/Laminas.java
+++ src/Laminas.java
@@ -1,3 +1,19 @@
-public class Laminas {
-    
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public class Laminas extends JPanel{
+    public Laminas(String titulo, String[] botones) {
+        setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), titulo));//Seteando borde con titulo pasado x parametros
+
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));// Creadno las cajas de texto, diciendole q sus items van en forma vertical
+        ButtonGroup buttons = new ButtonGroup();
+
+        for(int i =0; i<botones.length;i++){
+            JRadioButton boton = new JRadioButton(botones[i]);//creando los botones x caja de texto, estos ban dentro de un buttonGrup
+            add(boton);//agregando el boton a la lamina
+
+            buttons.add(boton);//agregando el boton a el BotonGrup
+            boton.setSelected(i==0);
+        }
+    }
 }
--- src/Marcos.java
+++ src/Marcos.java
@@ -3,14 +3,29 @@ import java.awt.*;
 
 
 public class Marcos extends JFrame{
+    private Laminas tipo;
+    // private Laminas
+    // private Laminas tipo;
+    // private Laminas tipo;
+    // private Laminas tipo;
+    // private Laminas tipo; 
 
     public Marcos(){
         setTitle("Prueba de Java");
         setBounds(500, 300,600, 450 );//dimenciones del panel
 
-        JPanel cuadricula = new JPanel();//Agregando una lamina simple
+        JPanel cuadricula = new JPanel();//creando una lamina simple
         cuadricula.setLayout(new GridLayout(2,3));
-        add(cuadricula);
+        
+
+        String primer[] = {"Mensaje", "Confirmar", "Opcion", "Entrada"};//Arreglo que contiene los nombres de los botones en esa cuadricula
+         
+
+        tipo = new Laminas("TIPO", primer);
+        cuadricula.add(tipo);//creando la lamina donde se muestran los botones
+
+        add(cuadricula);//agregando la cuadricula al marco
+
     }
     
 }
